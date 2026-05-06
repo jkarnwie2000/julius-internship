fe1602738296ce4ae0dbfed09d750300831d09ec
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-05-06 17:01:41 -0400

    animations run test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
@@ -15,6 +15,7 @@ function App() {
         <Route path="/explore" element={<Explore />} />
         <Route path="/author/:id" element={<Author />} />
         <Route path="/item-details/:id" element={<ItemDetails />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />
     </Router>
